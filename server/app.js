75a33f62ba0fb9d6109ac54ead0c97a4eeec08d6
Krishna kushwaha <63386697+krishh743@users.noreply.github.com>
2021-08-11 23:47:31 +0530

    third commit

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.get('/test', (req, res) => {
     res.status(200).send(arr)
 
 })
-
+// uses two parameter for request send and response recieve
 app.post('/postdat', async (req, res) => {
     const {name, age, address} = req.body
     const MenuData = new Menu({
@@ -33,7 +33,7 @@ app.get('/getdat', (req, res) => {
     const data = Menu.findById({_id: id})
     res.status(200).send(data)
 })
-
+// so here is start server on port 8080
 app.listen(8080, () => {
     console.log('server stared is 8080')
 })
